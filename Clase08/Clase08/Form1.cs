1eaecedaf14ed7d234da2862c7bc66cc5e2994d7
Josue Adrian <mendoza.l.josue.a@gmail.com>
2018-10-07 08:25:04 -0700

    Clase08
    Triangulo, Area, Perimetro, Hipotenusa

---
+++ Clase08/Clase08/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Operacion;//ESto es un name space, que referencia a otra clase
+
+namespace Clase08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ////Instanciamos el objeto para ver que podemos instanciar desde otra clase
+            //Calculo triangulo = new Calculo();
+            //triangulo.CalcularArea();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Objeto
+            Calculo triangulo = new Calculo();
+            //
+            triangulo.Base = Convert.ToDouble(textBox1.Text);
+            triangulo.Altura = double.Parse(textBox2.Text);
+
+            //MessageBox.Show("Base: " + triangulo.Base +" Altura: " + triangulo.Altura);
+
+            if (radioButton1.Checked)
+            {
+                var perimetro = triangulo.CalcularPerimetro(triangulo.Base, triangulo.Altura);
+                lblResultado.Text = "Resultado: " + Math.Round(perimetro, 3);
+            }
+
+            else if (radioButton2.Checked)
+            {   //Vamos a obtener area, con el metodo calcular area, y asignamos los parametros
+                var area = triangulo.CalcularArea(triangulo.Base, triangulo.Altura);
+                lblResultado.Text = "Resultado: " + area;
+            }
+            else if (radioButton3.Checked)
+            {
+                var hipotenusa = triangulo.CalcularHipotenusa(triangulo.Base, triangulo.Altura);
+                lblResultado.Text = "Resultado: " + Math.Round(hipotenusa, 3);
+            }
+
+        }
+    }
+
+}
---
+++ Clase08/Operacion/Calculo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operacion
+{
+    public class Calculo
+    { 
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        //Metodo para calcularArea
+        public double CalcularArea(double _base, double altura)
+        {
+            double area = (_base * altura)/2;
+            return area;
+        }
+
+        //Metodo con 2 parametros
+        public double CalcularHipotenusa(double _base, double altura)
+        {
+            double hipotenusa = Math.Sqrt((_base)*(_base) + (altura)*(altura));
+            return hipotenusa;//Hipotenusa
+        }
+
+        public double CalcularPerimetro(double _base, double altura)
+        {
+            //Instanciamos una variable, 
+            //double hipotenusa = CalcularHipotenusa(_base , altura);
+
+            double perimetro = CalcularHipotenusa(_base, altura) + _base + altura;
+
+            //Todo metodo con tipo de dato, debe devolver un valor
+            return perimetro;
+        }
+    }
+
+}
