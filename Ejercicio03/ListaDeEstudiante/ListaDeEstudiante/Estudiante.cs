4b83c45753af214def98d9a904d971a5507027fa
Josue Adrian <mendoza.l.josue.a@gmail.com>
2018-10-16 12:33:31 -0700

    Ejercicio03

---
+++ Ejercicio03/ListaDeEstudiante/ListaDeEstudiante/Estudiante.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeEstudiante
+{
+    class Estudiante
+    {
+        /* nombre
+         * apellido
+         * id
+         * semestre
+         * carrera
+         * edad
+         * sexo
+         * 
+         */
+        private int id;
+        private int edad;
+
+        private string nombre;
+        private string apellido;
+        private string semestre;
+        private string carrera;
+        private char sexo;
+
+
+        //se hacen publicas las variables, (como buena practica en programacion) 
+        //sirve para la seguridad en el programa
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Semestre
+        {
+            get { return semestre; }
+            set { semestre = value; }
+        }
+
+        public string Carrera
+        {
+            get { return carrera; }
+            set { carrera = value; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        public char Sexo
+        {
+            get { return sexo; }
+            set { sexo = value; }
+        }
+
+        public void VerInformacion(Estudiante student) //Pasando un atributo de tipo objeto
+        {
+            Console.WriteLine("Nombre Completo: " + student.Nombre +""+ student.Apellido);
+            Console.WriteLine("Numero de Control: " + student.Id);
+            Console.ReadLine();
+        }
+
+        public void VerInformacion(Estudiante student, string info) //sobrecarga de meto, Pasando un atributo de tipo objeto
+        {
+            Console.WriteLine("Nombre Completo: " + student.Nombre + "" + student.Apellido);
+            Console.WriteLine("Numero de Control: " + student.Id);
+            Console.WriteLine(info);
+            Console.ReadLine();
+        }
+
+
+
+
+    }
+}
---
+++ Ejercicio03/ListaDeEstudiante/ListaDeEstudiante/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeEstudiante
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            ///*
+            //Console.WriteLine("Hola Mundo");
+            //Console.ReadLine();
+
+            //Estudiante student = new Estudiante();
+            //student.Id = 2675;
+            //student.Nombre = "Javier";
+            //student.Apellido = "Rosas";
+
+            //Console.WriteLine(student.Nombre +" "+student.Apellido);
+            //Console.ReadKey();
+            //*/
+
+            //Console.Clear();
+            //Console.WriteLine("##### Bienvenidos a la lista de estudiantes #####");
+
+
+            //Console.WriteLine("Agrega un estudiante");
+            //Console.WriteLine("Cuando desee salir presione -1");
+
+            ////  string exit = Console.ReadLine();
+
+            //Console.Write("Defina su numero de estudiante: ");
+            //int studentNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Estudiante[] studentArray = new Estudiante[studentNumber]; //Arreglo, debemos tener un valor predefinido en el arreglo
+
+            //// int count = 0;
+
+            //for (int i = 0; i < studentArray.Length; i++)
+            //{
+
+            //    //count++;
+            //    //Inicializamos el objeto
+
+            //    Estudiante student = new Estudiante(); //instanciar estudiante
+            //    //Pedimos datos
+            //    Console.Write("Escriba el Nombre el estudiante: ");
+            //    student.Nombre = Console.ReadLine();
+
+            //    Console.Write("Escriba el Apellido el estudiante: ");
+            //    student.Apellido = Console.ReadLine();
+
+            //    Console.Write("Escriba el Id:");
+            //    student.Id = Convert.ToInt32(Console.ReadLine());
+            //    //Se convierte el dato capturado para que la variable Id pueda guardarlo
+
+            //    Console.Write("Escriba la Edad:");
+            //    student.Edad = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.Write("Escriba el Semestre:");
+            //    student.Semestre = Console.ReadLine();
+
+            //    Console.Write("Escriba el Sexo:");
+            //    student.Sexo = Convert.ToChar(Console.ReadLine());
+
+            //    studentArray[i] = student; //meter el estudiante en el arreglo
+
+
+            //    //studentArray[count] = student;
+            //    //Muestra el numero de estuiantes capturados, si se cierra el programa se borra
+
+            //    //Console.WriteLine("¿Desea agregar otro estudiante? ");
+            //    // exit = Console.ReadLine();
+            //}
+
+            //Console.WriteLine("Los estudiantes son: \n ");
+            //Console.WriteLine("Nombre \t Apellido \t Semestre \t Numero de Control");
+            //for (int i = 0; i < studentArray.Length; i++)
+            //{
+            //    Console.WriteLine(studentArray[i].Nombre + "\t" + studentArray[i].Apellido + "\t" + studentArray[i].Semestre + "\t" + studentArray[i].Id);
+            //    //studenyyArray[i].Nombre
+            //    //Arreglo [posicion].Atributo
+            //}
+            //Console.ReadKey();
+
+         
+
+            //Creamos un objeto
+            Estudiante student = new Estudiante();
+            student.Nombre = "Juan";
+            student.Apellido = "Vargas";
+            student.Id = 25;
+
+            //Debemos poner la lcase estudiante a publica para este ejercicio
+
+            Estudiante perfil = new Estudiante(); //Sobrecarga de metodo
+            perfil.VerInformacion(student);
+            perfil.VerInformacion(student, "Informacion presentada");
+
+        }
+    }
+}
+
